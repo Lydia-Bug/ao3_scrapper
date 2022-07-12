4496105a2f58318651e82d8f777c80f52ec09fd5
Lydia Acton <acton.lydia@gmail.com>
2022-07-13 11:05:39 +1200

    scatter plot now working

--- graph_data.py
+++ graph_data.py
@@ -5,15 +5,22 @@ import pandas as pd  ##Dependency
 ##from bokeh.palettes import magma ##Dependency
 import plotly.express as px ##Dependency
 
-data = pd.read_csv('scrapper.csv', index_col=0)
+data = pd.read_csv('test.csv', index_col=0)
 print(data.head(10))
 
 # instantiating the figure object
 # reading the database
 
-  
+words = data["words"]
+kudos = data["kudos"]
+
+## The x-axis has to be sorted
+words = [x for _,x in sorted(zip(words,kudos))]
+kudos = sorted(kudos)
+
+
 # plotting the scatter char
-fig = px.scatter(data, x="words", y="kudos")
+fig = px.scatter(x=words, y=kudos)
   
 # showing the plot
 fig.show()
